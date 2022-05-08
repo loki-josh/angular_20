c7e7f133e8254096eff8d86112633d871cddb408
loki-josh <lokijosh19@gmail.com>
2022-05-08 11:06:27 +0530

    Ice cream parlor page layout is ready and added all the requirements as per the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home/home.component';
+import { ProductsComponent } from './products/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
